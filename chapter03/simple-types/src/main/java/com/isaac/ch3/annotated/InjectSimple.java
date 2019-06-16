8e865e6b2c6def9483d5f52f0f38c18efb82f34c
isaac <535080691@qq.com>
2019-06-16 21:28:15 +0800

    chapter03——simple-types(contains spel)

---
+++ chapter03/simple-types/src/main/java/com/isaac/ch3/annotated/InjectSimple.java
@@ -0,0 +1,38 @@
+package com.isaac.ch3.annotated;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InjectSimple {
+    @Value("John Mayer")
+    private String name;
+    @Value("40")
+    private int age;
+    @Value("1.92")
+    private float height;
+    @Value("false")
+    private boolean programmer;
+    @Value("1241401112")
+    private Long ageInSeconds;
+
+    public static void main(String... args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-annotation.xml");
+        ctx.refresh();
+
+        InjectSimple simple = (InjectSimple) ctx.getBean("injectSimple");
+        System.out.println(simple);
+
+        ctx.close();
+    }
+
+    public String toString() {
+        return "Name: " + name + "\n"
+                + "Age: " + age + "\n"
+                + "Age in Seconds: " + ageInSeconds + "\n"
+                + "Height: " + height + "\n"
+                + "Is Programmer?: " + programmer;
+    }
+}
---
+++ chapter03/simple-types/src/main/java/com/isaac/ch3/xml/InjectSimple.java
@@ -0,0 +1,51 @@
+package com.isaac.ch3.xml;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class InjectSimple {
+    private String name;
+    private int age;
+    private float height;
+    private boolean programmer;
+    private Long ageInSeconds;
+
+    public static void main(String[] args){
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-simple-xml.xml");
+        ctx.refresh();
+
+        InjectSimple simple = (InjectSimple) ctx.getBean("injectSimple");
+        System.out.println(simple);
+
+        ctx.close();
+    }
+
+    public void setAgeInSeconds(Long ageInSeconds) {
+        this.ageInSeconds = ageInSeconds;
+    }
+
+    public void setProgrammer(boolean programmer) {
+        this.programmer = programmer;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Name: " + name + "\n"
+                + "Age: " + age + "\n"
+                + "Age in Seconds: " + ageInSeconds + "\n"
+                + "Height: " + height + "\n"
+                + "Is Programmer?: " + programmer;
+    }
+
+}
