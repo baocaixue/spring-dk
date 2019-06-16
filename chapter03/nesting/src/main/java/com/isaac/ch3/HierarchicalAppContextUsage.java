632d9d69f07b5d7e4ab5cd45ac8ab001b8b9a5cf
isaac <535080691@qq.com>
2019-06-16 21:17:21 +0800

    chapter03——nesting

---
+++ chapter03/nesting/src/main/java/com/isaac/ch3/HierarchicalAppContextUsage.java
@@ -0,0 +1,27 @@
+package com.isaac.ch3;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class HierarchicalAppContextUsage {
+    public static void main(String[] args){
+        GenericXmlApplicationContext parent = new GenericXmlApplicationContext();
+        parent.load("classpath:spring/parent-context.xml");
+        parent.refresh();
+
+        GenericXmlApplicationContext child = new GenericXmlApplicationContext();
+        child.load("classpath:spring/child-context.xml");
+        child.setParent(parent);
+        child.refresh();
+
+        Song song1 = (Song) child.getBean("song1");
+        Song song2 = (Song) child.getBean("song2");
+        Song song3 = (Song) child.getBean("song3");
+
+        System.out.println("from parent ctx: " + song1.getTitle());
+        System.out.println("from child ctx: " + song2.getTitle());
+        System.out.println("from parent ctx: " + song3.getTitle());
+
+        child.close();
+        parent.close();
+    }
+}
---
+++ chapter03/nesting/src/main/java/com/isaac/ch3/Song.java
@@ -0,0 +1,13 @@
+package com.isaac.ch3;
+
+public class Song {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
