3b7f398ff7b05f9392fed3ceabbb520a9a1ff51a
isaac <535080691@qq.com>
2019-06-26 22:19:13 +0800

    chapter05——spring AOP - throws advice

---
+++ chapter05/simple-throws-advice/src/main/java/com/isaac/ch5/ErrorBean.java
@@ -0,0 +1,11 @@
+package com.isaac.ch5;
+
+public class ErrorBean {
+    public void errorProneMethod() throws Exception {
+        throw new Exception("Generic Exception");
+    }
+
+    public void otherErrorProneMethod() throws IllegalArgumentException {
+        throw new IllegalArgumentException("IllegalArgument Exception");
+    }
+}
---
+++ chapter05/simple-throws-advice/src/main/java/com/isaac/ch5/SimpleThrowsAdvice.java
@@ -0,0 +1,46 @@
+package com.isaac.ch5;
+
+import org.springframework.aop.ThrowsAdvice;
+import org.springframework.aop.framework.ProxyFactory;
+
+import java.lang.reflect.Method;
+
+public class SimpleThrowsAdvice implements ThrowsAdvice {
+    public static void main(String... args) throws Exception {
+        ErrorBean errorBean = new ErrorBean();
+
+        ProxyFactory pf = new ProxyFactory();
+        pf.setTarget(errorBean);
+        pf.addAdvice(new SimpleThrowsAdvice());
+
+        ErrorBean proxy = (ErrorBean) pf.getProxy();
+
+        try {
+            proxy.errorProneMethod();
+        } catch (Exception ignored) {
+
+        }
+
+        try {
+            proxy.otherErrorProneMethod();
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    public void afterThrowing(Exception ex) throws Throwable {
+        System.out.println("***");
+        System.out.println("Generic Exception Capture");
+        System.out.println("Caught: " + ex.getClass().getName());
+        System.out.println("***\n");
+    }
+
+    public void afterThrowing(Method method, Object[] args, Object target,
+                              IllegalArgumentException ex) throws Throwable {
+        System.out.println("***");
+        System.out.println("IllegalArgumentException Capture");
+        System.out.println("Caught: " + ex.getClass().getName());
+        System.out.println("Method: " + method.getName());
+        System.out.println("***\n");
+    }
+}
