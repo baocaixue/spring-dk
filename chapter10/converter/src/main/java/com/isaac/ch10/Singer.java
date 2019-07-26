e168b0d9921afb68c0de1f7358552e0133628325
baocaixue <baocaixue@eorionsolution.com>
2019-07-26 16:42:57 +0800

    now you can using java8 time api

--- chapter10/converter/src/main/java/com/isaac/ch10/Singer.java
+++ chapter10/converter/src/main/java/com/isaac/ch10/Singer.java
@@ -1,21 +1,19 @@
 package com.isaac.ch10;
 
 import lombok.Data;
-import org.joda.time.DateTime;
 
 import java.net.URL;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 @Data
 public class Singer {
     private String firstName;
     private String lastName;
-    private DateTime birthDate;
+    private LocalDate birthDate;
     private URL personalSite;
 
     public String toString() {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return String.format("{First name: %s, Last name: %s, Birthday: %s, Site: %s}",
-                firstName, lastName, sdf.format(birthDate.toDate()), personalSite);
+                firstName, lastName, birthDate, personalSite);
     }
 }
